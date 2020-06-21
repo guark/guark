e0c8fcbd4350bbf1f494f6506cfa837112b631c1
Mohamed Elbahja <bm9qdw5r@gmail.com>
2020-06-21 23:26:20 +0100

    add comments, rename mode production.

--- app/watchers.go
+++ app/watchers.go
@@ -1,5 +1,10 @@
+// Copyright 2020 Mohammed El Bahja. All rights reserved.
+// Use of this source code is governed by a MIT license.
+
 package app
 
 type Watcher interface {
+
+	// Watcher routine.
 	Watch(*App)
 }
