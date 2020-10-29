72adfd1b5dd6209b94bb654e5cbd76528b07e2f8
Mohamed Elbahja <bm9qdw5r@gmail.com>
2020-10-30 00:18:25 +0100

    rename log interface.

--- log/log.go
+++ log/log.go
@@ -3,12 +3,71 @@
 
 package log
 
-type Log interface {
-	Debug(args ...interface{})
-	Info(args ...interface{})
-	Warn(args ...interface{})
-	Error(args ...interface{})
-	Fatal(args ...interface{})
-	Panic(args ...interface{})
-	SetLevel(level string)
+import (
+	"github.com/sirupsen/logrus"
+)
+
+type Log struct {
+	entry *logrus.Entry
+}
+
+func (l Log) Debug(args ...interface{}) {
+	l.entry.Debug(args...)
+}
+
+func (l Log) Info(args ...interface{}) {
+	l.entry.Info(args...)
+}
+
+func (l Log) Warn(args ...interface{}) {
+	l.entry.Warn(args...)
+}
+
+func (l Log) Error(args ...interface{}) {
+	l.entry.Error(args...)
+}
+
+func (l Log) Fatal(args ...interface{}) {
+	l.entry.Fatal(args...)
+}
+
+func (l Log) Panic(args ...interface{}) {
+	l.entry.Panic(args...)
+}
+
+func (l Log) SetLevel(n string) {
+
+	var level logrus.Level
+	switch n {
+	case "debug":
+		level = logrus.DebugLevel
+		break
+
+	case "info":
+		level = logrus.InfoLevel
+		break
+
+	case "error":
+		level = logrus.ErrorLevel
+		break
+
+	case "fatal":
+		level = logrus.FatalLevel
+		break
+
+	case "panic":
+		level = logrus.PanicLevel
+		break
+
+	default:
+		level = logrus.WarnLevel
+	}
+
+	logrus.SetLevel(level)
+}
+
+func New(label string) Logger {
+	return &Log{
+		entry: logrus.WithFields(logrus.Fields{"lebel": label}),
+	}
 }
